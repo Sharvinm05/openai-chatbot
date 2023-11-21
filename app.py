89f673dd9943aaf1c61f6cdbfdfd8e7dc67ca948
Sharvinm05 <73109293+Sharvinm05@users.noreply.github.com>
2023-11-22 07:12:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import streamlit as st
 load_dotenv()
 
-api_key = st.secrets["OPENAI_API_KEY"])
+api_key = st.secrets["OPENAI_API_KEY"]
 client = OpenAI(api_key=api_key)
 app = Flask(__name__)
 
